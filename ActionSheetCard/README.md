e9cde8e2c2e8456d84d1761d7bf8711264159163
Mahmud Ahsan <mahmud@thinkdiff.net>
2021-02-21 14:01:45 +0800

    Rearrange source files

--- ActionSheetCard/README.md
+++
@@ -1,3 +0,0 @@
-# ActionSheetCard
-
-A description of this package.
--- ActionSheetCard/Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import ActionSheetCardTests
-
-var tests = [XCTestCaseEntry]()
-tests += ActionSheetCardTests.allTests()
-XCTMain(tests)
